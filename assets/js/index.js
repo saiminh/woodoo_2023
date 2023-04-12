6c9854a7cf7d73075cf01a1889cf25b6a18171a1
saiminh <saiminh@gmail.com>
2023-04-12 17:16:51 +0200

    add currentLink

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,6 @@ function detectLinkToCurrentURL() {
   const currentURL = window.location.href;
   const links = document.querySelectorAll('a');
   links.forEach((link) => {
-    console.log(currentURL, link.href);
     if (link.href === currentURL) {
       link.classList.add('currentLink');
     }
